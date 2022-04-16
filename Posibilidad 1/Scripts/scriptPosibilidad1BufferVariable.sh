0bd0a11334a905e3f8a3987eb20f86a023b3054d
dolimpio <olimpiosilvadavid@gmail.com>
2022-04-16 22:57:23 +0200

    Graficas y archivos de datos añadidos para posibilidad 1

---
+++ Posibilidad 1/Scripts/scriptPosibilidad1BufferVariable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+java Posibilidad1 3 3 1
+java Posibilidad1 5 5 5
+java Posibilidad1 10 10 10
+java Posibilidad1 15 15 15
+java Posibilidad1 20 20 20
+java Posibilidad1 25 25 25
+java Posibilidad1 30 30 30
+java Posibilidad1 35 35 35
+java Posibilidad1 40 40 40
+java Posibilidad1 45 45 45
